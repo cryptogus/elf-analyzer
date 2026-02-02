1e02e4c5e832babf300a76269765aa7478d24d8e
cryptogus <wnelem1234@naver.com>
2026-02-02 18:33:16 +0900

    Add args codes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use goblin::elf::Elf;
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // // TEST args 1
+    // let args_iterator: std::env::Args = env::args();
+    // println!("{:?}", args_iterator);
+    // // TEST args 2
+    // let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+    
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <elf-file>", args[0]);
+        std::process::exit(1);
+    }
 }
