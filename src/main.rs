5c6ba29347b4aa70dca10c4025a02861fbb826bd
cryptogus <wnelem1234@naver.com>
2026-02-03 12:06:59 +0900

    Add elf parser

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,29 @@ fn main() {
         eprintln!("Usage: {} <elf-file>", args[0]);
         std::process::exit(1);
     }
+
+    // Read file
+    let path = &args[1];
+    let data = fs::read(path).expect("Can not read file");
+
+    // ELF parsing
+    let elf = Elf::parse(&data).expect("Fail to parsing ELF");
+
+    // Header information
+    println!("=== ELF Header ===");
+    println!("Entry point: 0x{:x}", elf.entry);
+    println!("Machine: {:?}", elf.header.e_machine);
+    println!("Type: {:?}", elf.header.e_type);
+    println!("Endian: {}", if elf.little_endian {"Little"} else {"Big"});
+
+    // Section
+    println!("\n=== Sections ===");
+    println!("{:<20} {:>12} {:>12}", "Name", "Size", "Addr");
+    println!("{}", "-".repeat(64));
+
+    for sh in &elf.section_headers {
+        let name = elf.shdr_strtab.get_at(sh.sh_name).unwrap_or("???");
+        println!("{:<20} {:>12} 0x{:08x}", name, sh.sh_size, sh.sh_addr);
+    }
+    
 }
